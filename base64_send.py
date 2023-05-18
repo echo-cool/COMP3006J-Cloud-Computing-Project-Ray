acee1d46debc7d313a3d241366054aa958901477
Yuyang Wang <yuyang.wang821@gmail.com>
2023-05-18 22:14:50 +0800

    dfsfd

--- base64_send.py
+++ base64_send.py
@@ -2,20 +2,28 @@ import base64
 import numpy as np
 import requests
 
-
+image_data_list = [
+    "zidane.jpg",
+    "mzx-1.jpg",
+    "mzx-2.jpg",
+    "mzx-3.jpg",
+    "mzx-4.jpg",
+    "mzx-5.jpg",
+]
 def main():
-    with open("test_data/zidane.jpg", "rb") as f:
-        image_base64 = base64.b64encode(f.read()).decode("utf-8")
-    response = requests.post(
-        "http://127.0.0.1:8000/detectBase64",
-        json={
-            'image_base64_data': image_base64,
-        },
-    )
-    # print(response.content)
-    print(response.status_code)
-    with open("test_data/zidane_result.jpg", "wb") as f:
-        f.write(response.content)
+    for image_name in image_data_list:
+        with open(f"test_data/{image_name}", "rb") as f:
+            image_base64 = base64.b64encode(f.read()).decode("utf-8")
+        response = requests.post(
+            "http://127.0.0.1:8000/detectBase64",
+            json={
+                'image_base64_data': image_base64,
+            },
+        )
+        # print(response.content)
+        print(response.status_code)
+        with open(f"test_data/{image_name}_result.jpg", "wb") as f:
+            f.write(response.content)
 
 
 if __name__ == '__main__':
